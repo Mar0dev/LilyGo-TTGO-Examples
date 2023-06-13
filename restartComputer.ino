908a5f6bce92a6a28af1e04356ea529ae571a1ea
Dominik <48558025+AlpenGod@users.noreply.github.com>
2023-06-13 19:53:16 +0200

    Changed delays

--- restartComputer.ino
+++ restartComputer.ino
@@ -3,7 +3,7 @@
 void setup() {
 
  Keyboard.begin(); //start keyboard communication 
-  delay(600);
+  delay(2000);
   Keyboard.press(KEY_LEFT_GUI);
   delay(600); 
   Keyboard.press('r');
@@ -22,7 +22,7 @@ void setup() {
   Keyboard.release('m');
   delay(300);
   Keyboard.release('d');
-  delay(1000);
+  delay(2000);
   Keyboard.press(KEY_RETURN);
   delay(600);
   Keyboard.release(KEY_RETURN);
